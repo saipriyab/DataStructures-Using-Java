41cb1f850777bb064a1061097ac52c306713a2a8
Saipriyadarshini Bandi <bsaipriyadarshini@gmail.com>
2022-01-14 19:09:19 +0530

    Maximum subarray using kadanes algorithm

---
+++ src/com/learning/arrays/MaximunSubarrayUsingKadanesAlgorithm.java
@@ -0,0 +1,20 @@
+package com.learning.arrays;
+
+public class MaximunSubarrayUsingKadanesAlgorithm {
+
+	public static void main(String[] args) {
+		int nums[] = { -2, 3, 1, -1, 4, 5 };
+
+		int currSum = nums[0], maxSum = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			currSum = currSum + nums[i];
+			if (currSum < nums[i])
+				currSum = nums[i];
+			if (maxSum < currSum)
+				maxSum = currSum;
+		}
+		System.out.println("maximum sum is" + maxSum);
+
+	}
+
+}
